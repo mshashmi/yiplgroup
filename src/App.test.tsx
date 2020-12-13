c33f7c5f161e59e961a66a2ad21bc6f50c7f9133
Sajid Hashmi <shashmi1996@gmail.com>
2020-12-13 20:05:28 +0530

    Updates

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Header from './component/Header';
-
-test('renders learn react link', () => {
-  render(<Header />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
